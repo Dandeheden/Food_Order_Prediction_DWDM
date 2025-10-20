7b34031b928bb08799b06b6af0ea8e807d5906de
Dandeheden <dandeheden433@gmail.com>
2025-10-20 22:06:22 +0530

    Update heden.py

--- heden.py
+++ heden.py
@@ -96,7 +96,6 @@ X_scaled = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
-
 # -----------------------------
 # Step 4: Train Random Forest Model
 # -----------------------------
